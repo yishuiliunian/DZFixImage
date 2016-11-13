91fdb156488302ace87d81751ab746eb910b644b
stonedong <yishuiliunian@gmail.com>
2016-11-13 14:42:09 +0800

    xxx

---
+++ Pod/Classes/DZFixImage.h
@@ -0,0 +1,14 @@
+//
+//  DZFixImage.h
+//  Pods
+//
+//  Created by stonedong on 16/11/13.
+//
+//
+
+#ifndef DZFixImage_h
+#define DZFixImage_h
+
+#import "UIImage+FixAppearance.h"
+
+#endif /* DZFixImage_h */
